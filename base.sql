5dc88468051f1cf23a30e94ce35ff7e3d0491632
tendrymbolanekena <tendrymbolanekena@gmail.com>
2026-07-20 09:18:30 +0300

    modif pour table v1

--- base.sql
+++ base.sql
@@ -1,49 +1,48 @@
-CREATE DATABASE IF NOT EXISTS mobileMoney;
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE prefixes (
-    idPrefixes INT AUTO_INCREMENT PRIMARY KEY,
-    valeur VARCHAR(10) NOT NULL,
-    operateur VARCHAR(50)
+    idPrefixes INTEGER PRIMARY KEY,
+    valeur TEXT NOT NULL UNIQUE, 
+    operateur TEXT NOT NULL     
 );
 
 CREATE TABLE typesOperations (
-    idTypesOperations INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(50) NOT NULL
+    idTypesOperations INTEGER PRIMARY KEY,
+    libelle TEXT NOT NULL UNIQUE 
 );
 
 CREATE TABLE frais (
-    idFrais INT AUTO_INCREMENT PRIMARY KEY,
-    montant1 DECIMAL(10,2) NOT NULL,
-    montant2 DECIMAL(10,2) NOT NULL,
-    valeurFrais DECIMAL(10,2) NOT NULL
-    -- idTypesOperations INT,
-    -- FOREIGN KEY (idTypesOperations) REFERENCES typesOperations(idTypesOperations)
+    idFrais INTEGER PRIMARY KEY,
+    montantMin INTEGER NOT NULL,            
+    montantMax INTEGER NOT NULL,            
+    valeurFrais INTEGER NOT NULL,           
+    idTypesOperations INTEGER,
+    FOREIGN KEY (idTypesOperations) REFERENCES typesOperations(idTypesOperations)
 );
 
 CREATE TABLE roles (
-    idRoles INT AUTO_INCREMENT PRIMARY KEY,
-    libelle VARCHAR(50) NOT NULL
+    idRoles INTEGER PRIMARY KEY,
+    libelle TEXT NOT NULL UNIQUE 
 );
 
 CREATE TABLE utilisateurs (
-    idUtilisateurs INT AUTO_INCREMENT PRIMARY KEY,
-    numeroTelephone VARCHAR(15) NOT NULL,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
-    idRoles INT,
+    idUtilisateurs INTEGER PRIMARY KEY,
+    numeroTelephone TEXT NOT NULL UNIQUE, 
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    idRoles INTEGER,
     FOREIGN KEY (idRoles) REFERENCES roles(idRoles)
 );
 
 CREATE TABLE operations (
-    idOperations INT AUTO_INCREMENT PRIMARY KEY,
-    montant DECIMAL(10,2) NOT NULL,
-    dateOperation DATETIME NOT NULL,
-    idTypesOperations INT,
-    idSource INT,
-    idDestinataire INT,
+    idOperations INTEGER PRIMARY KEY,
+    montant INTEGER NOT NULL,         
+    fraisAppliques INTEGER NOT NULL,  
+    dateOperation TEXT NOT NULL,       
+    idTypesOperations INTEGER,
+    idSource INTEGER,
+    idDestinataire INTEGER,
     FOREIGN KEY (idTypesOperations) REFERENCES typesOperations(idTypesOperations),
     FOREIGN KEY (idSource) REFERENCES utilisateurs(idUtilisateurs),
     FOREIGN KEY (idDestinataire) REFERENCES utilisateurs(idUtilisateurs)
 );
-
-
